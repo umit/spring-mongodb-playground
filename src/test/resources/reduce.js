73b6176490155890b2522ccc55250580d28e86e7
Ümit ÜNAL <umit.unal.89@gmail.com>
2012-05-01 17:43:03 +0300

    ignore file added

--- src/test/resources/reduce.js
+++ src/test/resources/reduce.js
@@ -6,14 +6,3 @@ function reduce(key,value){
     }
     return sum;
 }
-function map(){
-    emit(this.id,{price:this.price}); 
-} 
-function reduce(key,value){
-    var sum = 0;
-    for ( var i=0; i<value.length; i++ ) {
-
-            sum += value[i].price;
-    }
-    return sum;
-}
