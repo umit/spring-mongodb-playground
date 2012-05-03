0f0d2870506f01e554d27d699e9aacd9553f1ffe
Ümit ÜNAL <umit.unal.89@gmail.com>
2012-05-03 20:54:05 +0300

    remove "_class" field

--- src/main/java/com/umitunal/config/TestMongoConfiguration.java
+++ src/main/java/com/umitunal/config/TestMongoConfiguration.java
@@ -7,7 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
 @Profile("test")
@@ -18,9 +22,21 @@ public class TestMongoConfiguration {
     private @Value("${mongo.hostName}") String hostName;
     private @Value("${mongo.portNumber}") int portNumber;
 
-    public @Bean
-    MongoDbFactory mongoDbFactory() throws Exception {
+    public @Bean MongoDbFactory mongoDbFactory() throws Exception {
         return new SimpleMongoDbFactory(new Mongo(hostName, portNumber), databaseName);
     }
+    
+ 
+    public @Bean MongoTemplate mongoTemplate() throws Exception{
+    	return new MongoTemplate(mongoDbFactory(),mappingMongoConverter());
+    }
+    
+    public @Bean MappingMongoConverter mappingMongoConverter() throws Exception{
+    	MappingMongoConverter mappingMongoConverter = new MappingMongoConverter(mongoDbFactory(), new MongoMappingContext());
+    	mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+		return mappingMongoConverter;
+    }
+    
+    
 
 }
