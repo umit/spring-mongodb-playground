da2ec2260b7fa76c48ceccccf10e23c9d7555a18
Ümit ÜNAL <umit.unal.89@gmail.com>
2012-05-03 20:53:49 +0300

    map reduce

--- src/test/resources/map.js
+++ src/test/resources/map.js
@@ -1,3 +1,11 @@
 function map(){
     emit(this.id,{price:this.price}); 
 } 
+function reduce(key,value){
+    var sum = 0;
+    for ( var i=0; i<value.length; i++ ) {
+
+            sum += value[i].price;
+    }
+    return sum;
+}
